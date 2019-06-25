8bc8581cd9f9901488d75ea2c8aee5b7e4a4719d
Yc2034 <peiyaoshi0321@gmail.com>
2019-06-25 16:06:47 -0400

    luigi start

---
+++ spark/luigi/tasks.py
@@ -0,0 +1,9 @@
+'''
+Tasks define the operation logic for luigi
+
+Task defined here:
+- GamingTrack load data from S3, write output to postgres db
+- WritePostgres load data from GamingTrack, send result to postgres db
+'''
+
+
